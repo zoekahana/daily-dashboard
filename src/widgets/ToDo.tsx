acc44a995a3ce384a653cd0e6a52c416e3c47551
Zoe Kahana <zoekahana@hotmail.com>
2026-09-03 15:03:50 -0500

    destructure prop correctly

--- src/widgets/ToDo.tsx
+++ src/widgets/ToDo.tsx
@@ -45,7 +45,7 @@ const ToDoLabel = styled.label`
     min-height: 30px;
 `;
 
-const ToDoItem = ({ label: string }) => {
+const ToDoItem = ({label}: {label: string}) => {
 
     const [isCompleted, setIsCompleted] = useState(false);
 
