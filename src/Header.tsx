1e1da604c30810a314e3c64564ec37a465595d6c
Zoe Kahana <zoekahana@hotmail.com>
2026-09-11 00:54:40 -0500

    Base greeting and date string off of user's local time

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { toZonedTime, format } from 'date-fns-tz';
 
 const HeaderGrid = styled.div`
   display: grid;
@@ -33,10 +34,42 @@ const DateSubheader = styled.h2`
   }
 `
 
-const Header = () => 
-  <HeaderGrid>
-    <Greeting>Good morning, Zoe.</Greeting>
-    <DateSubheader>It's August 15, 2026.</DateSubheader>
-  </HeaderGrid>
+const MORNING = "Good morning, Zoe.";
+const AFTERNOON = "Good afternoon, Zoe.";
+const EVENING = "Good evening, Zoe.";
+const NIGHT = "Late night, Zoe.";
 
-export default Header;
+function getGreeting(hour: number): string {
+  console.log(hour);
+  if (hour >= 5 && hour < 12) return MORNING;
+  if (hour >= 12 && hour < 6) return AFTERNOON;
+  if (hour >= 6 && hour < 11) return EVENING;
+  return NIGHT;
+}
+
+function getDate(isoDateString: string): string {
+  return "It's " + format(isoDateString, "EEEE, MMMM dd") + ".";
+}
+
+export default function Header() {
+
+  const isoDate = new Date().toISOString();
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone; // Get user's timezone
+  const zonedDate = toZonedTime(isoDate, timeZone);
+  const hour = zonedDate.getHours();
+
+  const displayOptions = {
+    weekday: "long",
+    month: "long", 
+    day: "numeric"
+  };
+
+  const greeting = getGreeting(hour);
+  console.log(greeting);
+  const formattedDate = getDate(isoDate);
+
+  return <HeaderGrid>
+    <Greeting>{greeting}</Greeting>
+    <DateSubheader>{formattedDate}</DateSubheader>
+  </HeaderGrid>
+}
